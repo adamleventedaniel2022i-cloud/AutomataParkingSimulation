c215ef147d438f94216af00dad6c302169d9e2a7
menes.megyer.zeteny <menes.megyer.zeteny.2022i@bankitatabanya.hu>
2025-11-14 09:24:25 +0100

    CashRegister

--- Main/src/CashRegister.java
+++ Main/src/CashRegister.java
@@ -1,2 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class CashRegister {
+    private ArrayList<ArrayList<Integer>> denoms;
+
+
+    public ArrayList<ArrayList<Integer>> getDenoms() {
+        try ( BufferedReader br = new BufferedReader(new FileReader("balance.txt"));){
+            String line;
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            while((line = br.readLine())!=null){
+                StringTokenizer st = new StringTokenizer(line,"_");
+                System.out.println(line);
+            }
+
+
+            } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
 }
+
+
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,7 +1,25 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+    config();
+    CashRegister cr = new CashRegister();
+    cr.getDenoms();
+
+    }
+
+    public static void config()  {
+        File file = new File("balance.txt");
+        try {
+            FileWriter fw = new FileWriter("balance.txt");
+            fw.write("BALANCE=50000\nDENOM_2000_10\nDENOM_1000_10\nDENOM_500_20\nDENOM_200_40\nDENOM_100_100 ");
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
